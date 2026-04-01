3cbad10ca353eaf68f68e09f94842bd12bf6130f
Ankit Patel <apatel5907@gmail.com>
2026-04-01 14:15:49 -0500

    Updated maxSubarywithZeroSum

--- prefix_sum/max_Len_Subarray_with_Zero_Sum.py
+++ prefix_sum/max_Len_Subarray_with_Zero_Sum.py
@@ -9,6 +9,29 @@ Approach
 12.	When the same prefix sum recurs at index i, the subarray between first occurrence and i sums to 0.
 13.	Storing first (not latest) occurrence maximizes the subarray length.
 
+Output log:
+curr =  1 Num =  1 Index =  0
+prefix_idx =  {0: -1, 1: 0}
+curr =  0 Num =  -1 Index =  1
+prefix_idx[curr] =  -1  Max Len =  0
+Updated Max Len =  2
+curr =  3 Num =  3 Index =  2
+prefix_idx =  {0: -1, 1: 0, 3: 2}
+curr =  0 Num =  -3 Index =  3
+prefix_idx[curr] =  -1  Max Len =  2
+Updated Max Len =  4
+curr =  2 Num =  2 Index =  4
+prefix_idx =  {0: -1, 1: 0, 3: 2, 2: 4}
+curr =  2 Num =  0 Index =  5
+prefix_idx[curr] =  4  Max Len =  4
+Updated Max Len =  4
+curr =  0 Num =  -2 Index =  6
+prefix_idx[curr] =  -1  Max Len =  4
+Updated Max Len =  7
+curr =  2 Num =  2 Index =  7
+prefix_idx[curr] =  4  Max Len =  7
+Updated Max Len =  7
+7
 """
 
 def maxLenSubarrayZeroSum(nums):
@@ -18,13 +41,14 @@ def maxLenSubarrayZeroSum(nums):
  
     for i, num in enumerate(nums):
         curr += num
-        print ("curr = ", curr)
+        #print ("curr = ", curr, "Num = ", num, "Index = ", i)
         if curr in prefix_idx:
-            print("prefix_idx[curr] = ", prefix_idx[curr], " Max Len = ", max_len)
+            #print("prefix_idx[curr] = ", prefix_idx[curr], " Max Len = ", max_len)
             max_len = max(max_len, i - prefix_idx[curr])
+            #print("Updated Max Len = ", max_len)
         else:
             prefix_idx[curr] = i   # store FIRST occurrence only
-            print("prefix_idx[curr] = ", prefix_idx[curr])
+            #print("prefix_idx = ", prefix_idx)
  
     return max_len
  
